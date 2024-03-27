94568ab284be4fd12188161093729f7341edb283
nicho-UJN <nicholas9698@outlook.com>
2024-03-27 20:21:21 +0800

    fix getting sessionid

--- root/usr/share/campusnet/getSessionId.py
+++ root/usr/share/campusnet/getSessionId.py
@@ -2,7 +2,7 @@
 Author: nicho-UJN nicholas9698@outlook.com
 Date: 2024-03-19 12:59:31
 LastEditors: nicho-UJN nicholas9698@outlook.com
-LastEditTime: 2024-03-19 15:19:58
+LastEditTime: 2024-03-27 20:19:47
 FilePath: /luci-app-campusnet/getSessionId.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -22,8 +22,24 @@ def get_router_ip():
     
     return ip
 
+def get_base_ip():
+    base_ip = ""
+    try:
+        with open("/etc/config/campusnet", 'a') as f:
+            lines = f.readlines()
+            for line in lines:
+                items = line.split()
+                if items[1] == "base_ip":
+                    base_ip = items[2][1:-1]
+                    break
+    except Exception as e:
+        print(e)
+        return ""
+    return base_ip
+
+
 routerIP = get_router_ip()
-login_url = "http://139.198.3.98/sdjd/protalAction!index.action?wlanuserip="+routerIP+"&basip=124.128.40.39"
+login_url = "http://139.198.3.98/sdjd/protalAction!index.action?wlanuserip="+routerIP+"&basip="+get_base_ip()
 res = requests.get(login_url)
 cookies_org = res.cookies
 cookies = requests.utils.dict_from_cookiejar(cookies_org)
